b5f3dd62fd25824aa3b116ccaa6d4ec83c988b38
craig-j-walker <craig_j_walker@btinternet.com>
2025-01-01 11:57:07 +0000

    fixed mixup in day22

--- aoc2024/day22/part_one.cc
+++ aoc2024/day22/part_one.cc
@@ -1,15 +1,11 @@
 
-
 module;
 #include <bits/types/error_t.h>
 #include <cstdint>
-#include <iostream>
-#include <set>
 #include <sstream>
 #include <string>
 #include <string_view>
 #include <sys/types.h>
-#include <unordered_map>
 #include <vector>
 
 export module part_one;
@@ -18,74 +14,40 @@ using namespace std;
 
 export namespace part_one {
 
-// Define the graph using an adjacency list
-class Graph {
-public:
-  // Add an edge between two vertices
-  void addEdge(const string &src, const string &dest) {
-    adjList[src].push_back(dest);
-    adjList[dest].push_back(src); // For undirected graph
-  }
-
-  // Print the graph
-  void printGraph() const {
-    for (const auto &pair : adjList) {
-      cout << pair.first << " -> ";
-      for (const auto &vertex : pair.second) {
-        cout << vertex << " ";
-      }
-      cout << endl;
-    }
-  }
-
-  // Get 3-interconnected
-  set<set<string>> GetInterconnected() const {
-    set<set<string>> interconnected{};
-    for (const auto &vertex : adjList) {
-      for (const auto &vertex2 :
-           vertex.second) { // all vertices connected to vertex
-        for (const auto &vertex3 :
-             adjList.find(vertex2)
-                 ->second) { // all vertices connected to vertex2
-          for (const auto &other :
-               adjList.find(vertex3)
-                   ->second) { // all vertices connected to vertex3
-            if (other == vertex.first) {
-              interconnected.insert(set<string>{vertex2, vertex3, other});
-            }
-          }
-        }
-      }
-    }
-    return interconnected;
-  }
-
-private:
-  unordered_map<string, vector<string>> adjList;
-};
-
-void ParseInput(const string_view Input, Graph &G) {
+void ParseInput(const string_view Input,
+                vector<uint64_t> &InitialSecretNumbers) {
   stringstream ss((string(Input)));
   string line;
 
   while (getline(ss, line, '\n')) {
-    G.addEdge(line.substr(0, 2), line.substr(3, 2));
+    InitialSecretNumbers.push_back(stoll(line));
   }
 }
 
-uint64_t result(const string_view Input) {
-  Graph g{};
-  ParseInput(Input, g);
-  set<set<string>> interconnected = g.GetInterconnected();
-  uint64_t count = 0;
-  for (const auto &party : interconnected) {
-    for (const auto &computer : party) {
-      if (computer.contains("t")) {
-        count++;
-        break;
-      }
+void MixAndPrune(uint64_t &SecretNumber, uint64_t Result) {
+  SecretNumber = SecretNumber ^ Result;   // mix
+  SecretNumber = SecretNumber & 16777215; // prune
+}
+
+void Evolve(uint64_t &SecretNumber) {
+  uint64_t result = SecretNumber << 6;
+  MixAndPrune(SecretNumber, result);
+  result = SecretNumber >> 5;
+  MixAndPrune(SecretNumber, result);
+  result = SecretNumber << 11;
+  MixAndPrune(SecretNumber, result);
+}
+
+uint64_t result(const string_view input) {
+  vector<uint64_t> initialSecretNumbers{};
+  ParseInput(input, initialSecretNumbers);
+  uint64_t sum = 0;
+  for (auto &sn : initialSecretNumbers) {
+    for (int i = 0; i < 2000; i++) {
+      Evolve(sn);
     }
+    sum += sn;
   }
-  return count;
+  return sum;
 }
 } // namespace part_one
